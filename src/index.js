3b67bf7a46fab435c41935b829c6bca975ddb33a
Andres Hernandez <hernandez.andres.d@gmail.com>
2018-03-22 12:09:27 +1100

    First commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+
+export default class Person {
+
+  constructor( name ) {
+    this.name = name;
+  }
+
+  sayHello() {
+    return `Hello ${ this.name }!`;
+  }
+
+  sayHelloThreeTimes() {
+    let hello = this.sayHello();
+    return `${ hello } `.repeat(3);
+  }
+}
