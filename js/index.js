eb48ff0f30fa59b1c7641962a0eeaed5aa6b211f
Andres Hernandez <hernandez.andres.d@gmail.com>
2018-03-22 15:44:20 +1100

    Simplifies package.json file + Preprocess sass and babel + adds base styles

--- js/index.js
+++ js/index.js
@@ -1,34 +1,32 @@
 "use strict";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Person = function () {
-  function Person(name) {
-    _classCallCheck(this, Person);
+var newOne = function newOne() {
+  console.log("Hello World..!");
+};
+
+var Puzzle = function () {
+  function Puzzle(name) {
+    _classCallCheck(this, Puzzle);
 
     this.name = name;
   }
 
-  _createClass(Person, [{
-    key: "sayHello",
-    value: function sayHello() {
-      return "Hello " + this.name + "!";
-    }
-  }, {
-    key: "sayHelloThreeTimes",
-    value: function sayHelloThreeTimes() {
-      var hello = this.sayHello();
-      return (hello + " ").repeat(3);
+  _createClass(Puzzle, [{
+    key: "Name",
+    get: function get() {
+      return this.name;
+    },
+    set: function set(name) {
+      this.name = name;
     }
   }]);
 
-  return Person;
+  return Puzzle;
 }();
 
-exports.default = Person;
+var puzzle = new Puzzle('John Snow');
+console.log(puzzle.Name);
