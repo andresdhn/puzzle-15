70cea73143e6478be7759a9fbcc6fc2aadf33323
Andres Hernandez <hernandez.andres.d@gmail.com>
2018-03-23 17:06:10 +1100

    Gets random order of tiles from array + handle click event created to ignore blank tile click

--- js/index.js
+++ js/index.js
@@ -1,29 +1,73 @@
 'use strict';
 
+/**
+ * Puzzle 15.
+ *
+ * index.js
+ * @version 1.0.0
+ * @author [Andres Hernandez](https://github.com/andresdhn)
+ * 
+ */
+
 var puzzleSize = Math.pow(4, 2);
 var puzzle = document.getElementById('puzzle');
 
-var createTiles = function createTiles() {
-	for (var i = 1; i < puzzleSize + 1; i++) {
-		var tile = document.createElement('div');
+/*
+ * Shuffle Array  
+ */
 
-		if (i == puzzleSize) {
+var shuffleTiles = function shuffleTiles() {
+	return Array(puzzleSize).fill().map(function (e, i) {
+		return i + 1;
+	}).sort(function () {
+		return Math.random() - 0.5;
+	});
+};
+
+/*
+ * Initializes puzzle an injects tiles
+ */
+
+var initPuzzle = function initPuzzle() {
+	var tiles = shuffleTiles();
+
+	for (var i = 0; i < tiles.length; i++) {
+		var tile = document.createElement('div');
+		tile.id = tiles[i];
+
+		if (tiles[i] === puzzleSize) {
+			// last tile is blank
 			tile.classList.add('puzzle__tile', 'blank');
 		} else {
-			tile.innerHTML = i;
-			tile.classList.add('puzzle__tile', 'puzzle__tile--' + i);
+			// tile class and number
+			tile.innerHTML = tiles[i];
+			tile.classList.add('puzzle__tile', 'puzzle__tile--' + tiles[i]);
 		}
 
 		tile.addEventListener('click', function (e) {
-			return handleClick(e);
+			return handleTileClick(e);
 		});
 
 		puzzle.appendChild(tile);
 	}
 };
 
-var handleClick = function handleClick(e) {
-	console.log(e.target);
+/*
+ * Handles click event on tiles
+ */
+
+var handleTileClick = function handleTileClick(e) {
+
+	var selectedTile = e.target;
+
+	// Ignore click on blank tile  
+	if (selectedTile.id == puzzleSize) {
+		e.preventDefault();
+		e.stopPropagation();
+		return false;
+	}
+
+	console.log(selectedTile);
 };
 
-createTiles();
+initPuzzle();
